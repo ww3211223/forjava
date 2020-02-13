cfea100be3bcd6d5eea2014cd1d9c85f9016211b
nonono <ww3211223@163.com>
2020-02-13 17:11:51 +0800

    update: 修改stomp测试代码

--- 20191224_stomp_test/stomp_test_two/src/main/java/com/nonono/test/stomp/two/interceptors/HandleSharkeInterceptors.java
+++ 20191224_stomp_test/stomp_test_two/src/main/java/com/nonono/test/stomp/two/interceptors/HandleSharkeInterceptors.java
@@ -8,39 +8,39 @@ import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.util.Map;
 
-/**
- * 检查握手请求和响应，对WebSocketHandler传递属性
- */
-public class HandleSharkeInterceptors implements HandshakeInterceptor {
-
-    /**
-     * 在握手之前调用
-     *
-     * @param serverHttpRequest
-     * @param serverHttpResponse
-     * @param webSocketHandler
-     * @param map
-     * @return
-     * @throws Exception
-     */
-    @Override
-    public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
-        String name = ((ServletServerHttpRequest) serverHttpRequest).getServletRequest().getParameter("name");
-        System.out.println("========interceptors beforeHandshake:" + name);
-
-        return true;
-    }
-
-    /**
-     * 在握手之后调用
-     *
-     * @param serverHttpRequest
-     * @param serverHttpResponse
-     * @param webSocketHandler
-     * @param e
-     */
-    @Override
-    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
-
-    }
-}
+///**
+// * 检查握手请求和响应，对WebSocketHandler传递属性
+// */
+//public class HandleSharkeInterceptors implements HandshakeInterceptor {
+//
+//    /**
+//     * 在握手之前调用
+//     *
+//     * @param serverHttpRequest
+//     * @param serverHttpResponse
+//     * @param webSocketHandler
+//     * @param map
+//     * @return
+//     * @throws Exception
+//     */
+//    @Override
+//    public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
+//        String name = ((ServletServerHttpRequest) serverHttpRequest).getServletRequest().getParameter("name");
+//        System.out.println("========interceptors beforeHandshake:" + name);
+//
+//        return true;
+//    }
+//
+//    /**
+//     * 在握手之后调用
+//     *
+//     * @param serverHttpRequest
+//     * @param serverHttpResponse
+//     * @param webSocketHandler
+//     * @param e
+//     */
+//    @Override
+//    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
+//
+//    }
+//}
